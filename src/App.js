8c25e701a5547bdfc1b14ab0d2480833e42532b2
Anirup Patnaik <anirup049@gmail.com>
2021-07-05 20:35:54 +0530

    @mixin and @include

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar";
+import MainPage from "./Components/MainPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainPage />
     </div>
   );
 }
